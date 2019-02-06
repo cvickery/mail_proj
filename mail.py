21a277d0ab73fe1db2c041ce62173b8cdec6d857
Christopher Vickery <Christopher.Vickery@qc.cuny.edu>
2019-02-06 13:14:21 -0500

    Update mail.py
    Several enhancements.
    But the big deal is supplying the smtp server with the addresses of all
    recipients, not just the To: list.

--- mail.py
+++ mail.py
@@ -17,45 +17,86 @@ from email import message
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.utils import make_msgid, formatdate
+from email.headerregistry import Address
 import smtplib
+
+# Prefix for error reporting
+whoami = f'{sys.argv[0].strip("./")}: error:'
+
+
+def parse_addr_str(str):
+  """ Extract display_name, username and domain from a string.
+      Return an Address object
+  """
+  m = re.search(r'([^\<\s\@]+)@([^\s\>\@]+)', str)
+  if m is None:
+    return m
+  username = m[1]
+  domain = m[2]
+  display_name = str.replace(m[0], '').strip('> <')
+  return Address(display_name, username, domain)
+
+
+def build_addresses(addr_list):
+  """ Parse command line addresses and put them into proper structure for message headers.
+  """
+  return [parse_addr_str(str) for str in addr_list]
+
 
 parser = argparse.ArgumentParser(description='Simplified replacement for /usr/bin/mail',
                                  add_help=False)
 parser.add_argument('-?', '--help', action='help')
 parser.add_argument('-s', '--subject', default='Test Message')
-parser.add_argument('-cc', '--cc_addr', nargs='+')
-parser.add_argument('-bcc', '--bcc_addr', nargs='+')
+parser.add_argument('-c', '--cc_addr', nargs='+')
+parser.add_argument('-b', '--bcc_addr', nargs='+')
 parser.add_argument('-r', '--reply_addr')
 parser.add_argument('-h', '--html_file', type=argparse.FileType('r'))
 parser.add_argument('-p', '--plaintext_file', type=argparse.FileType('r'))
-parser.add_argument('-d', '--debuglevel', default=0)
-parser.add_argument('-f', '--from_addr', default=f"{os.getenv('USER')}@{os.getenv('HOSTNAME')}")
+parser.add_argument('-d', '--debuglevel', type=int, default=0)
+parser.add_argument('-f', '--from_addr',
+                    default=f"{os.getenv('USER')} <{os.getenv('USER')}@{os.getenv('HOSTNAME')}>")
 parser.add_argument('to_addr', nargs='+')
 args = parser.parse_args()
 
+# Be sure all recipients have valid email addresses; build list of all recipents
+if parse_addr_str(args.from_addr) is None:
+  print(f'{whoami} “{args.from_addr}” is not a valid return address')
+  exit(1)
+
+all_recipients = []
+for recipient in args.cc_addr, args.bcc_addr, args.reply_addr, args.to_addr:
+  if type(recipient) == list:
+    for r in recipient:
+      if parse_addr_str(r) is None:
+        print(f'{whoami} “{r}” is not a valid email address', file=sys.stderr)
+        exit(1)
+      else:
+        all_recipients += [r]
+  if type(recipient) == str:
+    if parse_addr_str(recipient) is None:
+      print(f'{whoami} “{r}” is not a valid email address', file=sys.stderr)
+      exit(1)
+    else:
+      all_recipients += [recipient]
+all_recipents = ', '.join(all_recipients)
+
+# There has to be an SMTP server
 smtp_server = os.getenv('SMTP_SERVER')
 if smtp_server is None:
   smtp_server = os.getenv('HOSTNAME')
-
-# Be sure the from_addr contains something that looks like it might be an email address.
-#   Otherwise, argparse lets an empty string get through, and then the mailer doesn't complain,
-#   leaving the user in the dark.
-from_addr = args.from_addr
-if re.search(r'\S+@\S+', from_addr) is None:
-  print(f'{sys.argv[0].strip("./")}: error: invalid from_addr', file=sys.stderr)
-  exit(1)
-
 try:
-  debuglevel = int(args.debuglevel)
-except ValueError:
-  print(f'{sys.argv[0].strip("./")}: error: debuglevel must be an int', file=sys.stderr)
+  server = smtplib.SMTP(smtp_server)
+except socket.gaierror as err:
+  print(f'{whoami} unable to connect to smtp server “{smtp_server}”: {err}',
+        file=sys.stderr)
   exit(1)
+server.set_debuglevel(args.debuglevel)
 
 # Set up the message parts.
 msg = MIMEMultipart('alternative')
 msg['Message-ID'] = make_msgid()
 msg['Date'] = formatdate(datetime.now().timestamp())
-msg['From'] = f'{from_addr}'
+msg['From'] = args.from_addr
 msg['Subject'] = args.subject
 msg['To'] = ', '.join(args.to_addr)
 if args.cc_addr is not None:
@@ -91,12 +132,5 @@ if args.html_file is not None:
   msg.attach(MIMEText(html_body, 'html'))
 
 # Send the message
-try:
-  server = smtplib.SMTP(smtp_server)
-except socket.gaierror as err:
-  print(f'{sys.argv[0].strip("./")}: error: unable to connect to smtp server {smtp_server}: {err}',
-        file=sys.stderr)
-  exit(1)
-server.set_debuglevel(debuglevel)
-server.sendmail(from_addr, args.to_addr, msg.as_string())
+server.sendmail(args.from_addr, all_recipients, msg.as_string())
 server.quit()
